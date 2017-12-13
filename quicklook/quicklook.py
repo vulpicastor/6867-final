31e87100b256f4ac8a8c83edd8a07174b20f6a2d
Lizhou Sha <slz@mit.edu>
2017-12-13 01:31:56 -0500

    quicklook.py: Refactor find_time_range to reject incomplete transits

--- quicklook/quicklook.py
+++ quicklook/quicklook.py
@@ -10,8 +10,11 @@ import traceback
 
 import injection
 
-BOUND_TOL = 6. / 24  # day; this is equivalent to 12 long cadences.
+# Whether to randomize padding. if True, will be randomly selected between
+# BOUND_TOL_LOWER and BOUND_TOL_UPPER. If False, will use fixed BOUND_TOL.
+# True for LSTM, False for SVM.
 RANDOMIZE_BOUND = True
+BOUND_TOL = 6. / 24  # day; this is equivalent to 12 long cadences.
 BOUND_TOL_UPPER = 1.  # day
 BOUND_TOL_LOWER = 3./24  # day
 NEG_UPPER = 1.  # day; upper bound for time span of negative samples.
@@ -31,36 +34,38 @@ def dictify(fits_header):
             out[k] = None
     return out
 
-def find_time_range(fits_table_header, injected_row):
-    epoch = injected_row["i_epoch"]  # mid-transit reference epoch.
-    period = injected_row["i_period"]  # transit period (i.e. how often it repeats).
-    dur = injected_row["i_dur"] / 24.  # transit duration in days.
-    # Find the start and stop times of the light curve.
-    t_start, t_stop = fits_table_header["TSTART"], fits_table_header["TSTOP"]
+def find_time_range(table_start, table_stop, epoch, period, dur, name="Unknown"):
     # Find how many periods since epoch is the transit in the light curve.
-    num_period = (t_start - epoch) // period + 1  # VERY IMPORTANT OFF BY ONE.
+    num_period = (table_start - epoch) // period + 1  # VERY IMPORTANT OFF BY ONE.
     # Calculate the start and stop times of the transit.
     mid_transit = epoch + period * num_period  # Time of mid-transit
+    half_width = dur / 2.
+    transit_start = mid_transit - half_width
+    transit_stop = mid_transit + half_width
+    # literally the transit don't exist in the data.
+    if table_start >= transit_stop or table_stop <= transit_start:
+        logging.error("Light curve range contains no transit for %s!", name)
+        return None, None, None, None
+    # Sanity checking.
     if RANDOMIZE_BOUND:
         # For LSTM, padding should be randomized in order to prevent overfitting based on
         # when the transit starts in the light curve.
-        half_width = dur / 2.
-        start = mid_transit - half_width - random.range(BOUND_TOL_LOWER, BOUND_TOL_UPPER)
-        stop = mid_transit + half_width + random.range(BOUND_TOL_LOWER, BOUND_TOL_UPPER)
+        start = transit_start - random.uniform(BOUND_TOL_LOWER, BOUND_TOL_UPPER)
+        stop = transit_stop + random.uniform(BOUND_TOL_LOWER, BOUND_TOL_UPPER)
     else:
-        half_width = dur / 2. + BOUND_TOL  # Include an extra hour (12 long cadences)
-        start = mid_transit - half_width
-        stop = mid_transit + half_width
-    logging.info("The start and stop period for {} is {}, {}".format(fits_table_header["OBJECT"], start, stop))
+        # Constant padding otherwise.
+        start = transit_start - BOUND_TOL
+        stop = transit_stop - BOUND_TOL
+    # Check that transit is not clipped by data boundary
+    if table_start >= start or table_stop <= stop:
+        logging.error("Transit partially outside light curve range for %s!", name)
+        return None, None, None, None
+    logging.info("The start and stop period for {} is {}, {}".format(name, start, stop))
     return start, stop, mid_transit, dur
 
 def strip_rows(fits_table, t_start, t_stop):
     rows = []
     header, data = fits_table.header, fits_table.data
-    # Make sure that the given start and stop times are actually within bounds
-    # of the light curve.
-    if header["TSTART"] >= t_start or header["TSTOP"] <= t_stop:  # Assumes that table is sorted by time.
-        logging.warning("t_start or t_stop outside of light curve bounds!")
     for r in data:
         if t_start <= r["TIME"] and r["TIME"] <= t_stop:
             rows.append(r)
@@ -103,16 +108,24 @@ def gather_data(filename, injected_table, injected_table_index):
     with fits.open(filename) as hdulist:
         # Extract KIC ID part of the file name.
         kic_id = injection.parse_injected_filename(filename)
-        # Find corresponding row in the injected data table.
-        injected_row = injected_table[injected_table_index[kic_id]]
+        injected_i = injected_table_index[kic_id]
+        # Extract row metadata
+        epoch = injected_table["i_epoch"][injected_i]
+        period = injected_table["i_period"][injected_i]
+        dur = injected_table["i_dur"][injected_i] / 24.  # hour
+        table_start = hdulist[1].header["TSTART"]
+        table_stop = hdulist[1].header["TSTOP"]
         # Find when the transit starts and stops in the FITS file.
-        start, stop, mid_transit, dur = find_time_range(hdulist[1].header, injected_row)
+        start, stop, mid_transit, dur = find_time_range(table_start, table_stop, epoch, period, dur, name=hdulist[1].header["OBJECT"])
+        # When find_time_range fails, you know you are fucked.
+        if start is None:
+            return None, None
         # Strip out the rows that are actually transts.
         rows = strip_rows(hdulist[1], start, stop)
         # Also generate the negative samples
         neg_rows = strip_rows_negative(hdulist[1], start, stop)
         # Extract information of whether an eclipsing binary is being simulated
-        is_eb = injected_row["EB_injection"]
+        is_eb = injected_table["EB_injection"][injected_i]
         # Only preserve the TIME and SAP_FLUX columns of the light curve.
         # Also add tag for whether the mid transit point has passed.
         t = strip_cols(rows, dictify(hdulist[1].header), mid_transit, is_eb, dur)
@@ -125,7 +138,7 @@ def gather_data(filename, injected_table, injected_table_index):
 def main():
     logging.warning("Making sure that warning shots are fired")
     # Load the injected transits data table.
-    injected = injection.parse_injected_table("/mnt/data/meta/kplr_dr25_inj3_plti.txt")
+    injected = injection.parse_injected_table("/mnt/data/meta/kplr_dr25_inj1_plti.txt")
     index = injection.index_injected_table(injected)
     # Iterate through all filenames given as command line arguments.
     for i in sys.argv[1:]:
@@ -136,7 +149,8 @@ def main():
             # "/mnt/data/INJ1/kplr011183555-2011271113734_INJECTED-inj1_llc.fits.gz"
             t, neg_t = gather_data(filename, injected, index)
             root = path.basename(filename).split(".")[0]
-            ascii.write(t, root + "_quicklook.ecsv", format='ecsv')
+            if t is not None:
+                ascii.write(t, root + "_quicklook.ecsv", format='ecsv')
             if neg_t is not None:
                 ascii.write(neg_t, root + "_quicklook_negative.ecsv", format='ecsv')
         except Exception as e:
